493bcd52d6c5f7a26d7df19ee361790809c0158a
Sebb <ecbbadel@gmail.com>
2024-10-14 15:00:05 +0200

    w

--- js/villas.js
+++ js/villas.js
@@ -1,3 +1,4 @@
-function parama_js(){
-    window.open('https://www.villanovo.com/villa-rentals/europe/france/french-riviera/eze/villa-pamara?acode=aUfhoCvT')
-}
+function parama_js(){window.open('https://www.villanovo.com/villa-rentals/europe/france/french-riviera/eze/villa-pamara?acode=aUfhoCvT')}
+function olympe_js(){window.open('https://www.villanovo.com/villa-rentals/europe/france/french-riviera/cassis/villa-olympe?acode=aUfhoCvT')}
+function canest_js(){window.open('https://www.villanovo.com/villa-rentals/europe/france/french-riviera/saint-tropez/canest?acode=aUfhoCvT')}
+function angelique_js(){window.open('https://www.villanovo.com/villa-rentals/europe/france/french-riviera/eze/villa-angelique?acode=aUfhoCvT')}
